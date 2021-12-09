fbd2a5f20b07908bf483919533739f5ab9b5a6f6
Alex Chunet <achunet@worldbank.org>
2021-12-09 16:26:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # main.py
 
-from flask import Flask, send_file
 from selenium import webdriver
 import chromedriver_binary  # Adds chromedriver binary to path
-
-app = Flask(__name__)
+import pandas as pd
+from sodapy import Socrata
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 # The following options are required to make headless Chrome
 # work in a Docker container
@@ -17,9 +20,40 @@ chrome_options.add_argument("--no-sandbox")
 # Initialize a new browser
 browser = webdriver.Chrome(chrome_options=chrome_options)
 
+client = Socrata("finances.worldbank.org", '4lAjROKl9GysVT07fl34yIlL4', username="achunet@worldbank.org", password="19920JOkeR19920")
 
-@app.route("/")
-def hello_world():
-    browser.get("https://www.google.com/search?q=headless+horseman&tbm=isch")
-    browser.save_screenshot("spooky.png")
-    return send_file("spooky.png")
+results = client.get("3y7n-xmbj", limit=2000)
+
+# Convert to pandas DataFrame
+results_df = pd.DataFrame.from_records(results)
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+url = 'http://projects.worldbank.org/procurement/noticeoverview?id=OP00157448'
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get(url)
+html = driver.page_source
+driver.close()
+
+soup = BeautifulSoup(html, features="html.parser")
+
+# kill all script and style elements
+for script in soup(["script", "style"]):
+    script.extract()    # rip it out
+
+# get text
+text = soup.get_text()
+
+# break into lines and remove leading and trailing space on each
+lines = (line.strip() for line in text.splitlines())
+# break multi-headlines into a line each
+chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+# drop blank lines
+text = ' '.join(chunk for chunk in chunks if chunk)
+
+if "Contract" in text:
+    print("query found")
+else:
+    print("no match")
