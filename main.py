837bc472d3e6b5858b29704af7d485ce73b031fd
Alex Chunet <achunet@worldbank.org>
2023-11-15 10:46:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from bs4 import BeautifulSoup
 import smtplib
 import dash
 import dash_bootstrap_components as dbc
-import dash_html_components as html
+from dash import html
 import gunicorn
 import signal
 
