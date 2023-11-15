4206a299ca75a86661929481f0580a610585789d
Alex Chunet <achunet@worldbank.org>
2023-11-15 11:30:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,12 @@ chrome_options.add_argument("window-size=1024,768")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
+print("Options added")
+
 # Initialize a new browser
 browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+
+print("Browser initialized")
 
 @app.route("/")
 def hello_world():
